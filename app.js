dc9a241780cf4fe7d42340c4fb61642c718fd62e
RobertoSants <j-uni-nho25@hotmail.com>
2025-10-29 16:43:01 -0300

    Atualização com novo estilo de CSS e novas páginas EJS criadas dentro do diretório views.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 //Servir arquivos estáticos da pasta 'public'
 app.use(express.static('public'));
 
-//Rota principal
+//Rota principal (Home)
 app.get('/', (req, res) => {
     res.render('index', { title: 'Minha Aplicação Express'});
 });
@@ -17,6 +17,16 @@ app.get('/', (req, res) => {
 //Outras Rotas
 app.get('/sobre', (req, res) => {
     res.render('index', { title: 'Sobre Nós'});
+});
+
+//Rota para "Serviços"
+app.get('/servicos', (req, res) => {
+    res.render('servicos', { title: 'Serviços' });
+});
+
+//Rota para "Contato"
+app.get('/contato', (req, res) => {
+    res.render('contato', { title: 'Contato' });
 });
 
 //Iniciar o Servidor
